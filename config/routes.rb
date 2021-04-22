7d04247bfc25e5853efa5034a113108594dc5ed6
Vivian <vivian.vuong@live.ca>
2021-04-22 02:47:30 -0500

    Added Product and Category controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'category/index'
+  get 'category/show'
+  get 'product/index'
+  get 'product/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
