9ec17357a22bac19729a29bfeecbe084fe013d6f
Vivian <vivian.vuong@live.ca>
2021-04-22 01:38:33 -0500

    Added validate to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates :name, :price, :photo_image, :description, presence: true
+  validates :title, uniqueness
+  validates :price, numericality: {only_integers: true}
 end
