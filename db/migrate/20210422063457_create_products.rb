f152ebc0fe7e06b3ffafdfd9f473d3c86635cf7a
Vivian <vivian.vuong@live.ca>
2021-04-22 01:35:46 -0500

    Added Product model

---
+++ db/migrate/20210422063457_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.string :photo_image
+      t.text :description
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
